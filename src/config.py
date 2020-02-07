107d20add75dfebb5e4ecb2a6a8d31f2735c0ab1
mnichol3 <nicholson.matt94@gmail.com>
2020-02-07 17:04:49 -0500

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,8 @@
+"""
+A class to hold configuration information used by the frozen emission scripts
+
+Matt Nicholson
+7 Feb 2020
+"""
+import yaml
+
---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""
+Main script that produces the frozen emissions
+
+Matt Nicholson
+7 Feb 2020
+"""
+# import logging
+
+import logger
+import ceds_io
+import config
+
+
+
+
+
+def main():
+    print('Not implemented yet!')
+    
+if __name__ == '__main__':
+    main()
