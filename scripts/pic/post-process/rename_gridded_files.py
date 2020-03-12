47d9cc61a6a2310c3bc38caf6e667bd3d1eb5658
mnichol3 <nicholson.matt94@gmail.com>
2020-03-12 16:30:10 -0400

    Dynamically get file date

--- scripts/pic/post-process/rename_gridded_files.py
+++ scripts/pic/post-process/rename_gridded_files.py
@@ -26,21 +26,24 @@ print('Changing working directory to {}'.format(ROOT_DIR))
 os.chdir(ROOT_DIR)
 
 species_pattern = re.compile(r'^(\w{2,5})-em')
-fnames = {'anthro' : '{}-em-anthro_input4MIPs_emissions_CEDS-2017-05-18-frozen-US-EF_gn_201001-201412.nc',
-          'biofuel': '{}-em-SOLID-BIOFUEL-anthro_input4MIPs_emissions_CEDS-2017-05-18-frozen-US-EF-supplemental-data_gn_196001-200912.nc'
+dates_pattern = re.compile(r'gn_(\d{6}-\d{6})\.nc$')
+fnames = {'anthro' : '{}-em-anthro_input4MIPs_emissions_CEDS-2017-05-18-frozen-US-EF_gn_{}.nc',
+          'biofuel': '{}-em-SOLID-BIOFUEL-anthro_input4MIPs_emissions_CEDS-2017-05-18-frozen-US-EF-supplemental-data_gn_{}.nc'
           }
 
 # Get the names of every file in the current directory with the '.nc' extension
 grid_files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f) and f[-3:] == '.nc']
 
 for grid_file in grid_files:
-    match = species_pattern.search(grid_file)
-    if match:
-        species = match.group(1)
+    match1 = species_pattern.search(grid_file)
+    match2 = dates_pattern.search(grid_file)
+    if match1 and match2:
+        species = match1.group(1)
+        dates = match2.group(1)
         if 'BIOFUEL' in grid_file:
             new_fname = fnames['biofuel']
         else:
             new_fname = fnames['anthro']
-        new_fname = new_fname.format(species)
+        new_fname = new_fname.format(species, dates)
         print('{} --> {}'.format(grid_file, new_fname))
         os.rename(grid_file, new_fname)
