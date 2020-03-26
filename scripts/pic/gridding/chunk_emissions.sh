eaeee4811f5bbe504bd4547951185f1345b89ad2
mnichol3 <matthew.nicholson@pnnl.gov>
2020-03-26 16:51:24 -0400

    Add solidbiofule chunking

--- scripts/pic/gridding/chunk_emissions.sh
+++ scripts/pic/gridding/chunk_emissions.sh
@@ -11,20 +11,43 @@
 module purge
 module load R/3.3.3
 
-#Actually codes starts here
+# Actually codes starts here
 now=$(date)
 echo "Current time : $now"
 
 cd /pic/projects/GCAM/mnichol/ceds/worktrees/CEDS-frozen-em
 
+# --- BC ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R BC --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R BC --nosave --no-restore
+
+# --- CO ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R CO --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R CO --nosave --no-restore
+
+# --- CO2 ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R CO2 --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R CO2 --nosave --no-restore
+
+# --- NH3 ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R NH3 --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R NH3 --nosave --no-restore
+
+# --- NMVOC ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R NMVOC --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R NMVOC --nosave --no-restore
+
+# --- NOx ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R NOx --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R NOx --nosave --no-restore
+
+# --- OC ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R OC --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R OC --nosave --no-restore
+
+# --- SO2 ---
 Rscript code/module-G/G2.1.chunk_bulk_emissions.R SO2 --nosave --no-restore
+Rscript code/module-G/G2.4.chunk_solidbiofuel_emissions.R SO2 --nosave --no-restore
 
 now=$(date)
 echo "Current time : $now"
