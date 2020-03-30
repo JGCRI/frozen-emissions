eb5fa4f20c66d2e1b1375b3ea073302b7b22b6dd
mnichol3 <matthew.nicholson@pnnl.gov>
2020-03-30 12:04:13 -0400

    Change working dir

--- scripts/make_final_emissions.py
+++ scripts/make_final_emissions.py
@@ -16,12 +16,15 @@ import os
 import sys
 
 # Fetch CEDS path CMD arg
-ceds_path = sys.argv[1]
+ceds_dir = sys.argv[1]
 
 # Path of summary script within the CEDS directory
 script_path = 'code/module-S/S1.1.write_summary_data.R'
 
 species = ['BC', 'CH4', 'CO', 'CO2', 'NH3', 'NMVOC', 'NOx', 'OC', 'SO2']
+
+print('Changing working directory to {}'.format(ceds_dir))
+os.chdir(ceds_dir)
 
 for specie in species:
     # Execute the summary script for each emissions species defined in the above list
