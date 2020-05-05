133dd9aef8d9b13c5ab291712f1d88aeef0f8e87
Matt Nicholson <matthew.nicholson@pnnl.gov>
2020-05-05 09:53:25 -0400

    Initial commit; archive files script

---
+++ scripts/pic/post-process/archive_files.py
@@ -0,0 +1,11 @@
+"""
+Move CEDS annual gridded, final emissions, and final gridded emissions files to
+an archive directory.
+
+Matt Nicholson
+24 April 2020
+"""
+from __future__ import print_function
+import os
+import sys
+import re
